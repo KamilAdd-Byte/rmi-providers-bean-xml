9e70d33f3c999917cb14f48a6a52b550e3b2887a
KamilAdd-Byte <kamillodzinski09@gmail.com>
2022-01-07 15:58:32 +0100

    0.1 Generated project

---
+++ src/main/java/pl/rmi/rmiprovidersrest/RmiProvidersRestApplication.java
@@ -0,0 +1,13 @@
+package pl.rmi.rmiprovidersrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmiProvidersRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmiProvidersRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rmi/rmiprovidersrest/RmiProvidersRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rmi.rmiprovidersrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmiProvidersRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
